bd3a8d3ce98ffc0072f10658cb2f6a422890629e
= <kyroschow@gmail.com>
2018-07-27 00:49:43 -0700

    Added 3 UIViewControllers

---
+++ StarterProject/C5G1-App/Controllers/ChineseIntroController.swift
@@ -0,0 +1,14 @@
+//
+//  ChineseIntroController.swift
+//  iOS
+//
+//  Created by Cluster 5 on 7/27/18.
+//  Copyright © 2018 MBIENTLAB, INC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChineseIntroController: UIViewController {
+    
+}
---
+++ StarterProject/C5G1-App/Controllers/HomePageController.swift
@@ -0,0 +1,14 @@
+//
+//  HomePageController.swift
+//  iOS
+//
+//  Created by Cluster 5 on 7/27/18.
+//  Copyright © 2018 MBIENTLAB, INC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomePageController: UIViewController {
+    
+}
