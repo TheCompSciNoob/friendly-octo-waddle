34cb78145b5eac3daf0c743190540a7af01f453c
MatthewClark <mgclark007@gmail.com>
2018-08-02 16:05:30 -0700

    started on english stuff

--- StarterProject/C5G1-App/Controllers/EnglishIntroController.swift
+++ StarterProject/C5G1-App/Controllers/EnglishIntroController.swift
@@ -13,8 +13,44 @@ class EnglishIntroController: UIViewController {
     
     var device: MBLMetaWear? = nil
     var soundManager: SoundManager!
+    var parthTimer: Timer!
     
     override func viewDidLoad() {
         loadMapBackground(root: self.view)
+        soundManager = SoundManager(fileNames: [], options: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // start device sending updates
+        device?.sensorFusion?.eulerAngle.startNotificationsAsync { (obj, error) in
+            self.soundManager?.updateAngularOrientation(degreesYaw: abs(Float(360 - (obj?.y)!)), degreesPitch: Float((obj?.p)!), degreesRoll: Float((obj?.r)!))
+        }
+        
+        //init timers
+        parthTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(moveSounds), userInfo: nil, repeats: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // stop updates from device
+        device?.sensorFusion?.eulerAngle.stopNotificationsAsync()
+        
+        // stop timer
+        parthTimer.invalidate()
+    }
+    
+    // 0: story (parth)
+    // 1: campfire
+    // 2: laugh track (lol)
+    // where are footsteps
+    
+    func initPositions() {
+        // change volume and positions of the sounds and person
+        soundManager.changeVolume(index: 0, vol: 1)
+        soundManager.updateListenerPosition(x: 0, y: 0, z: 0)
+        
+    }
+    
+    @objc func moveSounds() {
+        
     }
 }
